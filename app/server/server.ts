e562c5f4697dbcf1efabce255db929f8a830dc58
Batubo Victory <victorybatubo76@gmail.com>
2026-04-06 05:39:29 +0530

    test: Test API endpoint

--- app/server/server.ts
+++ app/server/server.ts
@@ -4,4 +4,8 @@ const PORT = 8000
 const app = express()
 app.use(express.json())
 
+app.get('/', (req, res) => {
+    res.json({ message: 'API working'})
+})
+
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`))
