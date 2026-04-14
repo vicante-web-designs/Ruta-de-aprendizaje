acaa00b4e1821596aa9567aa66aaae9de78e3aec
Batubo Victory <victorybatubo76@gmail.com>
2026-04-14 12:18:25 +0100

    feat: Testing get route

--- project_1/server.js
+++ project_1/server.js
@@ -2,4 +2,8 @@ const express = require('express')
 const app = express()
 const PORT = 8383
 
+app.get('/', (req, res) => {
+    res.json({message: 'Thank you'})
+})
+
 app.listen(PORT, () => console.log(`Server is running at: ${PORT}`))
