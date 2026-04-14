472a28b0a330b4353758878fee5b6b8e74374ef2
Batubo Victory <victorybatubo76@gmail.com>
2026-04-14 13:28:51 +0100

    feat: Testing delete endpoint

--- project_1/server.js
+++ project_1/server.js
@@ -2,13 +2,26 @@ const express = require('express')
 const app = express()
 const PORT = 8383
 
-const data = {
-    name: 'Victory'
-}
+let data = [
+    {
+        name: 'james',
+        email: 'james@example.com'
+    },
+]
+
+// middleware
+app.use(express.json())
 
 //WEBSITE endpoints
 app.get('/', (req, res) => {
-  res.send('<h1>Homepage</h1>')
+  res.send(`
+     <body style='background:pink;color:blue;'>
+        <h1>DATA:</h1>
+        <p>
+            ${JSON.stringify(data)}
+        </p>
+     </body>
+    `)
 })
 
 app.get('/dashboard', (req, res) => {
@@ -18,6 +31,19 @@ app.get('/dashboard', (req, res) => {
 //API endpoints
 app.get('/api/data', (req, res) => {
   res.send(data)
+})
+
+app.post('/api/users', (req, res) => {
+  const newData = req.body
+  console.log(newData)
+  data.push(newData)
+  res.status(201).json(data)
+})
+
+app.delete('/api/users', (req, res) => {
+  data.pop()
+
+  res.status(200).json(data)
 })
 
 app.listen(PORT, () => console.log(`Server is running at: ${PORT}`))
