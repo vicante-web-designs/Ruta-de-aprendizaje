ac748da59ba81518bdfd4ce621bb95b26710d2c8
Batubo Victory <victorybatubo76@gmail.com>
2026-04-14 12:56:40 +0100

    feat: Test api endpoint

--- project_1/server.js
+++ project_1/server.js
@@ -2,13 +2,22 @@ const express = require('express')
 const app = express()
 const PORT = 8383
 
+const data = {
+    name: 'Victory'
+}
+
+//WEBSITE endpoints
 app.get('/', (req, res) => {
-    res.json({message: 'Thank you', confirmation: 'Yay, I hit an endpoint'})
+  res.send('<h1>Homepage</h1>')
 })
 
 app.get('/dashboard', (req, res) => {
-    console.log('Oooooohhhhhh, now i hit the dashboard endpoint')
-    res.json({message: 'Oooooohhhhhh, now i hit the dashboard endpoint'})
+  res.send('<h1>Dashboard</h1>')
+})
+
+//API endpoints
+app.get('/api/data', (req, res) => {
+  res.send(data)
 })
 
 app.listen(PORT, () => console.log(`Server is running at: ${PORT}`))
