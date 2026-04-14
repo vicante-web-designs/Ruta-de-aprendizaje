10fecb09334635284d9e52e3bc693b2efe7e868b
Batubo Victory <victorybatubo76@gmail.com>
2026-04-14 12:40:38 +0100

    feat: Test get endpoint

--- project_1/server.js
+++ project_1/server.js
@@ -3,7 +3,12 @@ const app = express()
 const PORT = 8383
 
 app.get('/', (req, res) => {
-    res.json({message: 'Thank you'})
+    res.json({message: 'Thank you', confirmation: 'Yay, I hit an endpoint'})
+})
+
+app.get('/dashboard', (req, res) => {
+    console.log('Oooooohhhhhh, now i hit the dashboard endpoint')
+    res.json({message: 'Oooooohhhhhh, now i hit the dashboard endpoint'})
 })
 
 app.listen(PORT, () => console.log(`Server is running at: ${PORT}`))
