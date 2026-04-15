ae6c245f338e23eea58eff732701c787b61835d7
Batubo Victory <victorybatubo76@gmail.com>
2026-04-15 14:33:45 +0100

    feat: Configure database

--- project_2/src/server.js
+++ project_2/src/server.js
@@ -1,13 +1,21 @@
-import dotenv from 'dotenv'
-dotenv.config()
 import express from 'express'
+import path, {dirname} from 'path'
+import { fileURLToPath } from 'url'
+
 const app = express()
 const PORT = process.env.PORT || 5000
 
-app.use(express.json())
+const __fileName = fileURLToPath(import.meta.url)
+const __dirName = dirname(__fileName)
 
+//Middlewares
+// Serves our code from the public directory
+app.use(express.json())
+app.use(express.static(path.join(__dirName, '../public')))
+
+// Serving up the HTML file from the directory
 app.get('/', (req, res) => {
-  res.status(200)
+  res.sendFile(path.join(__dirName, 'public', 'index.html'))
 })
 
 app.listen(PORT, () => console.log(`Listening to server: ${PORT}`))
